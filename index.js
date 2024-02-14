66aff14cb1ed8e85580d987bcf659dff9a831292
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-14 13:48:06 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Assuming 'helpers.js' provides necessary setup or utilities but not shown here
+// require('./helpers.js'); // Uncomment this in actual code if helpers.js is needed
+
+// scuberGreetingForFeet function
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// ternaryCheckCity function
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// switchOnCharmFromTip function
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+// These functions are designed to pass the tests described in your file.
